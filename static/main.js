f7a6d6b9e7cf0418c77154b23bc9a623a5ab7f3d
Vincent Tavernier <vince.tavernier@gmail.com>
2021-04-15 17:20:57 +0200

    Add mermaid

---
+++ static/main.js
@@ -0,0 +1,15 @@
+if (document.querySelector(".mermaid")) {
+  const script = document.createElement("script");
+  script.onload = function () {
+    const config = {
+      startOnLoad: true,
+      theme: "dark",
+      themeVariables: {
+        fontSize: '12px',
+      },
+    };
+    mermaid.initialize(config);
+  };
+  script.src = "https://unpkg.com/mermaid@8.9.2/dist/mermaid.min.js";
+  document.head.appendChild(script);
+}
