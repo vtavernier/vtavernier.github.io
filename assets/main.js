1360056d7543b2c41f597bfd091f528316579108
Vincent Tavernier <v.tavernier@pm.me>
2023-02-05 22:28:45 +0100

    feat: update mermaid to 9.1.3

--- assets/main.js
+++ assets/main.js
@@ -5,11 +5,11 @@ if (document.querySelector(".mermaid")) {
       startOnLoad: true,
       theme: "dark",
       themeVariables: {
-        fontSize: '12px',
+        fontSize: "12px",
       },
     };
     mermaid.initialize(config);
   };
-  script.src = "https://unpkg.com/mermaid@8.13.3/dist/mermaid.min.js";
+  script.src = "https://unpkg.com/mermaid@9.1.3/dist/mermaid.min.js";
   document.head.appendChild(script);
 }
