dbe0abdc1151f5af704daa5f431fe0107ec320f5
Vincent Tavernier <v.tavernier@pm.me>
2023-02-27 22:43:28 +0100

    chore: fix prettierignore

--- src/utils/frontmatter.mjs
+++ src/utils/frontmatter.mjs
@@ -2,7 +2,7 @@ import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
 export function readingTimeRemarkPlugin() {
-  return function(tree, file) {
+  return function (tree, file) {
     const textOnPage = toString(tree);
     const readingTime = Math.ceil(getReadingTime(textOnPage).minutes);
 
