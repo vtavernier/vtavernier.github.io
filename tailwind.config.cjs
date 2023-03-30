04969c481de8c4656b9f68e3ae4824a8226eff51
Vincent Tavernier <v.tavernier@pm.me>
2023-03-30 23:50:25 +0200

    fix(blog): reduce font size for pre

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
@@ -31,6 +39,10 @@ module.exports = {
             'blockquote p:last-of-type::after': {
               content: '',
             },
+            'pre': {
+              fontSize: em(22, 24),
+              lineHeight: round(22 / 16),
+            }
           },
         },
       },
